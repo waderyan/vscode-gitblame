f82a7cdc274f41faac1c7ecff189ff2fd4335342
Albin Jacobsson <albin.jacobsson@gmail.com>
2019-07-27 17:02:48 +0200

    Fixing test-runner

--- test/run-test.ts
+++ test/run-test.ts
@@ -8,17 +8,23 @@ async function main(): Promise<void> {
     try {
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
-        const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+        const extensionDevelopmentPath = path.resolve(__dirname, '../');
 
         // The path to the extension test script
         // Passed to --extensionTestsPath
         const extensionTestsPath = path.resolve(
             __dirname,
-            './test/suite/index',
+            './suite/index',
         );
 
         // Download VS Code, unzip it and run the integration test
-        await runTests({ extensionDevelopmentPath, extensionTestsPath });
+        await runTests({
+            extensionDevelopmentPath,
+            extensionTestsPath,
+            launchArgs: [
+                "--disable-extensions",
+            ],
+        });
     } catch (err) {
         process.exit(1);
     }
