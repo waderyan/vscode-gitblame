2aab8a5fe7bedcb371f8ea2ab6d448d6beaf9872
Albin Jacobsson <albin.jacobsson@gmail.com>
2021-07-15 10:50:03 +0200

    Add test to not regress #110

--- test/suite/generate-url-tokens.test.ts
+++ test/suite/generate-url-tokens.test.ts
@@ -125,4 +125,52 @@ suite("Generate URL Tokens", () => {
         assert.strictEqual(call(tokens["project.remote"]), "github.com/Sertion/vscode-gitblame");
         assert.strictEqual(call(tokens["file.path"]), "/fake.file");
     });
+
+    test("ssh://git@ origin", async () => {
+        const activeEditorStub = stub(getActive, "getActiveTextEditor");
+        const execcommandStub = stub(execcommand, "execute");
+        const propertyStub = stub(property, "getProperty");
+        activeEditorStub.returns({
+            document: {
+                isUntitled: false,
+                fileName: "/fake.file",
+                uri: Uri.parse("/fake.file"),
+            },
+            selection: {
+                active: {
+                    line: 1,
+                },
+            },
+        });
+        execcommandStub.withArgs(match.string, ["symbolic-ref", "-q", "--short", "HEAD"], match.object)
+            .resolves("master");
+        execcommandStub.withArgs(match.string, ["config", "branch.master.remote"], match.object)
+            .resolves("origin");
+        execcommandStub.withArgs(match.string, ["config", "remote.origin.url"], match.object)
+            .resolves("ssh://git@github.com/Sertion/vscode-gitblame.git");
+        execcommandStub.withArgs(match.string, ["ls-remote", "--get-url", "origin"], match.object)
+            .resolves("ssh://git@github.com/Sertion/vscode-gitblame.git");
+        execcommandStub.withArgs(match.string, ["ls-files", "--full-name", "--", "/fake.file"], match.object)
+            .resolves("/fake.file");
+        propertyStub.withArgs("remoteName").returns("origin");
+
+        const [origin, tokens] = await generateUrlTokens(exampleCommit);
+
+        activeEditorStub.restore();
+        execcommandStub.restore();
+        propertyStub.restore();
+
+        assert.strictEqual(origin, "ssh://git@github.com/Sertion/vscode-gitblame.git");
+
+        assert.strictEqual(call(tokens["gitorigin.hostname"], ""), "github.com");
+        assert.strictEqual(call(tokens["gitorigin.hostname"], "0"), "github");
+        assert.strictEqual(call(tokens["gitorigin.hostname"], "1"), "com");
+        assert.strictEqual(call(tokens["gitorigin.path"], ""), "/Sertion/vscode-gitblame");
+        assert.strictEqual(call(tokens["gitorigin.path"], "0"), "Sertion");
+        assert.strictEqual(call(tokens["gitorigin.path"], "1"), "vscode-gitblame");
+        assert.strictEqual(call(tokens["hash"]), "60d3fd32a7a9da4c8c93a9f89cfda22a0b4c65ce");
+        assert.strictEqual(call(tokens["project.name"]), "vscode-gitblame");
+        assert.strictEqual(call(tokens["project.remote"]), "github.com/Sertion/vscode-gitblame");
+        assert.strictEqual(call(tokens["file.path"]), "/fake.file");
+    });
 });
